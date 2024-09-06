1316bf56ff3667f5d657693d7895bdfc3ee001db
0xJus <stennettjustin55@gmail.com>
2024-09-05 20:15:54 -0400

    Added checks for account creation

--- src/main.py
+++ src/main.py
@@ -7,55 +7,7 @@ import string
 import logging
 
 
-months = [
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december",
-]
-
-
 async def main():
-    # Start the browser
-    driver = await uc.start()
-
-    # Open the webpage
-    page = await driver.get("https://account.jagex.com/en-GB")
-
-    # need to verify cloudflare
-    # let's handle the cookie nag as well
-    verify_cloudflare = await page.select("input[type=checkbox]")
-    if verify_cloudflare:
-        await verify_cloudflare.click()
-
-    # let's handle the cookie nag as well
-    cookie_bar_accept = await page.find("Allow all cookies", best_match=True)
-    if cookie_bar_accept:
-        await cookie_bar_accept.click()
-
-    time.sleep(2)
-
-    # Click "Create an account"
-    create_account = await page.find("Create an account", best_match=True)
-    if create_account:
-        await create_account.click()
-
-    time.sleep(2)
-
-    # Enter email
-    email = await page.select("input[type=email]")
-    randstr = lambda k: "".join(random.choices(string.ascii_letters, k=k))
-    await email.send_keys("".join([randstr(8), "@", randstr(8), ".com"]))
-
-    time.sleep(2)
 
     # Generate random values
     random_month = random.randint(1, 12)
@@ -69,34 +21,71 @@ async def main():
     random_day_str = str(random_day).zfill(2)
     random_year_str = str(random_year)
 
+    # Start the browser
+    driver = await uc.start()
+
+    # Open the webpage
+    page = await driver.get("https://account.jagex.com/en-GB")
+
+    # need to verify cloudflare
+    # let's handle the cookie nag as well
+    verify_cloudflare = await page.query_selector("button")
+    if verify_cloudflare:
+        await verify_cloudflare.click()
+
+    # let's handle the cookie nag as well
+    cookie_bar_accept = await page.find("Allow all cookies", best_match=True)
+    if cookie_bar_accept:
+        await cookie_bar_accept.click()
+
+    # Click "Create an account"
+    create_account = await page.find("Create an account", best_match=True)
+    if create_account:
+        await create_account.click()
+
+    # Enter email
+    email = await page.select("input[type=email]")
+    if email:
+        randstr = lambda k: "".join(random.choices(string.ascii_letters, k=k))
+        await email.send_keys("".join([randstr(8), "@", randstr(8), ".com"]))
+
     print('finding the "month", "day", and "year" select elements')
 
     # Look for the month, day, and year input fields using their name attributes
     sel_day = await page.query_selector('input[name="day"]')
-    await sel_day.send_keys(random_day_str)
+    if sel_day:
+        await sel_day.send_keys(random_day_str)
 
     sel_month = await page.query_selector('input[name="month"]')
-    await sel_month.send_keys(random_month_str)
+    if sel_month:
+        await sel_month.send_keys(random_month_str)
 
     sel_year = await page.query_selector('input[name="year"]')
-    await sel_year.send_keys(random_year_str)
+    if sel_year:
+        await sel_year.send_keys(random_year_str)
 
-    time.sleep(3)
+    # Look for the tos & agreement polocies with the specified ID
+    # Then find and click the element
+    tos_agreement_checkbox = await page.query_selector(
+        "#registration-start-accept-agreements"
+    )
+    if tos_agreement_checkbox:
+        await tos_agreement_checkbox.click()
 
-    email = await page.select("input[type=email]")
-    randstr = lambda k: "".join(random.choices(string.ascii_letters, k=k))
-    await email.send_keys("".join([randstr(8), "@", randstr(8), ".com"]))
+    # Look for the continue button and click it
+    continue_button = await page.find("Continue", best_match=True)
+    if continue_button:
+        await continue_button.click()
 
+    # Sleep for 5 seconds manually
     time.sleep(5)
 
-    # # Look for the element with the specified ID
 
-    # tos_agreement_checkbox = await page.query_selector(
-    #     'input[name="registration-start-accept-agreements"]'
-    # )
-
-    # # Click the element
-    # await tos_agreement_checkbox.click()
+# START OF VERIFICATION PROCESS
+async def verification_process():
+    loggin.info("Starting verification process")
+    # need to log into gmail and collect verify code
+    # display it by either a discord webhook or return the verification code after viewing/scraping the burner email
 
 
 # print('filling in the "password" input field')
@@ -107,4 +96,6 @@ async def main():
 
 
 if __name__ == "__main__":
+    # since asyncio.run never worked (for me)
+    # i use
     uc.loop().run_until_complete(main())
